124341957e3aef14a7b24deb417511d8aa13ca6c
rybinsky <bar1noff.n@yandex.ru>
2023-04-15 00:00:11 +0300

    Перенес часть в utils

--- utils.py
+++ utils.py
@@ -3,6 +3,107 @@ import numpy as np
 import pandas as pd
 import copy
 
+
+class Node:
+    def __init__(self, value = None, prev = None, next = None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+class DoublyLinkedList:
+    def __init__(self, max_size = 5):
+        try:
+            if max_size <= 0:
+                raise ValueError(f"'max_size' должен быть больше 0")
+            self.head = None
+            self.tail = None
+            self.size = 0
+            self.max_size = max_size
+        except ValueError as e:
+            print(e)
+
+    def is_empty(self):
+        return self.size == 0
+
+    def __len__(self):
+        return self.size
+
+    def __lpush(self, value):
+        try:
+            if self.size == self.max_size:
+                raise ValueError(f"Достигнут максимальный размер списка!")
+            new_node = Node(value)
+            if self.is_empty():
+                self.head = self.tail = new_node
+            else:
+                new_node.next = self.head
+                self.head.prev = new_node
+                self.head = new_node
+            self.size += 1
+        except ValueError as e:
+            print(e)
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.size == self.max_size:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+            self.head = self.head.next
+            self.head.prev = None
+        else:
+            if self.is_empty():
+                self.head = self.tail = new_node
+            else:
+                new_node.prev = self.tail
+                self.tail.next = new_node
+                self.tail = new_node
+            self.size += 1
+
+    def pop(self):
+        if self.is_empty():
+            raise Exception("Список пуст!")
+        removed_node = self.head
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+        self.size -= 1
+        return removed_node.value
+
+    def __rpop(self):
+        if self.is_empty():
+            raise Exception("Список пуст!")
+        removed_node = self.tail
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        self.size -= 1
+        return removed_node.value
+    
+    def resize(self, new_max_size):
+        '''
+        Изменяет МАКСИМАЛЬНЫЙ допустимый размер списка
+        '''
+        if self.size < new_max_size:
+            pass
+        elif self.size > new_max_size:
+            for _ in range(new_max_size, self.size):
+                self.__rpop()
+            self.size = new_max_size
+        self.max_size = new_max_size
+
+    def print_dll(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.value)
+            current_node = current_node.next
+
+
+
 def _iqr_outliers_percent(df: pd.DataFrame, columns, threshold):
     '''
     Приватная функция выводит процент выбросов в столбцах columns матрицы признаков df
